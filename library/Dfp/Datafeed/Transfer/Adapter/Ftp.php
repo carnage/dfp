d7ec9c0a0350d871e92d8da87d13540755e80a41
Carnage <t.carnage@gmail.com>
2013-04-02 00:08:34 +0100

    Refactored Ftp adapter to use uri component, refactored ftp adapter to be more testable.

--- library/Dfp/Datafeed/Transfer/Adapter/Ftp.php
+++ library/Dfp/Datafeed/Transfer/Adapter/Ftp.php
@@ -32,23 +32,9 @@ class Dfp_Datafeed_Transfer_Adapter_Ftp extends Dfp_Datafeed_Transfer_Adapter_Ab
     /**
      * Contains the ftp connection resource.
      *
-     * @var string
+     * @var resource
      */
     protected $_ftp;
-
-    /**
-     * Holds the hostname of the server to connect to
-     *
-     * @var string
-     */
-    protected $_host;
-
-    /**
-     * Holds the port to connect to the server on
-     *
-     * @var string
-     */
-    protected $_port = '21';
 
     /**
      * Contains the timeout time in seconds.
@@ -58,20 +44,12 @@ class Dfp_Datafeed_Transfer_Adapter_Ftp extends Dfp_Datafeed_Transfer_Adapter_Ab
     protected $_timeout = 90;
 
     /**
-     * Holds the username to connect to the server with. The username 'anonymous' is default.
-     *
-     * @var string
+     * Use passive mode FTP
+     * 
+     * @var boolean
      */
-    protected $_username = 'anonymous';
-
-    /**
-     * Holds the password to use when connecting to the server. By convention an
-     * email address for the user should be used as the password.
-     *
-     * @var string
-     */
-    protected $_password = '';
-
+    protected $_passive = false;
+    
     /**
      * Holds the base path to use for locating files locally. Files will be sent from and saved to this location.
      *
@@ -95,26 +73,6 @@ class Dfp_Datafeed_Transfer_Adapter_Ftp extends Dfp_Datafeed_Transfer_Adapter_Ab
     }
 
     /**
-     * Getter for host
-     *
-     * @return string
-     */
-    public function getHost()
-    {
-        return $this->_host;
-    }
-
-    /**
-     * Getter for port
-     *
-     * @return string
-     */
-    public function getPort()
-    {
-        return $this->_port;
-    }
-
-    /**
      * Getter for timeout
      *
      * @return string
@@ -122,26 +80,6 @@ class Dfp_Datafeed_Transfer_Adapter_Ftp extends Dfp_Datafeed_Transfer_Adapter_Ab
     public function getTimeout()
     {
         return $this->_timeout;
-    }
-
-    /**
-     * Getter for username
-     *
-     * @return string
-     */
-    public function getUsername()
-    {
-        return $this->_username;
-    }
-
-    /**
-     * Getter for password
-     *
-     * @return string
-     */
-    public function getPassword()
-    {
-        return $this->_password;
     }
 
     /**
@@ -154,67 +92,20 @@ class Dfp_Datafeed_Transfer_Adapter_Ftp extends Dfp_Datafeed_Transfer_Adapter_Ab
         return $this->_basePath;
     }
 
-    /**
-     * Setter for host
-     *
-     * @param string $host
-     * @return Dfp_Datafeed_Transfer_Adapter_Ftp
-     */
-    public function setHost($host)
+    public function getPassive()
     {
-        $this->_host = $host;
-        return $this;
+    	return $this->_passive;
     }
-
-    /**
-     * Setter for port
-     *
-     * @param string $port
-     * @return Dfp_Datafeed_Transfer_Adapter_Ftp
-     */
-    public function setPort($port)
-    {
-        $this->_port = $port;
-        return $this;
-    }
-
+    
     /**
      * Setter for timeout
      *
      * @param int $timeout
-     * @param boolean True if system is under test
      * @return Dfp_Datafeed_Transfer_Adapter_Ftp
      */
-    public function setTimeout($timeout, $test = false)
+    public function setTimeout($timeout)
     {
         $this->_timeout = $timeout;
-        if ($test == false && is_resource($this->_ftp)) {
-            ftp_set_option($this->_ftp, FTP_TIMEOUT_SEC, $this->getTimeout());
-        }
-        return $this;
-    }
-
-    /**
-     * Setter for username
-     *
-     * @param string $username
-     * @return Dfp_Datafeed_Transfer_Adapter_Ftp
-     */
-    public function setUsername($username)
-    {
-        $this->_username = $username;
-        return $this;
-    }
-
-    /**
-     * Setter for password
-     *
-     * @param string $password
-     * @return Dfp_Datafeed_Transfer_Adapter_Ftp
-     */
-    public function setPassword($password)
-    {
-        $this->_password = $password;
         return $this;
     }
 
@@ -237,17 +128,40 @@ class Dfp_Datafeed_Transfer_Adapter_Ftp extends Dfp_Datafeed_Transfer_Adapter_Ab
      * Sets if passive (true) or active (false) should be used.
      *
      * @param boolean $passive
-     * @param boolean True if system is under test
      * @return Dfp_Datafeed_Transfer_Adapter_Ftp
      */
-    public function setPassive($passive, $test = false)
+    public function setPassive($passive)
     {
-        if (false === $test) {
-            @ftp_pasv($this->_getFtp(), (bool) $passive);
-        }
+    	$this->_passive = (bool) $passive;
         return $this;
     }
 
+    /**
+     * Returns the uri object
+     *
+     * @throws Dfp_Datafeed_Transfer_Exception
+     * @return Dfp_Datafeed_Transfer_Uri
+     */
+    public function getUri()
+    {
+    	if (is_null($this->_uri)) {
+    		$this->_uri = new Dfp_Datafeed_Transfer_Uri();
+    	}
+    	return $this->_uri;
+    }
+    
+    /**
+     * Setter for uri
+     *
+     * @param Dfp_Datafeed_Transfer_Uri $uri
+     * @return Dfp_Datafeed_Transfer_Adapter_Stream
+     */
+    public function setUri(Dfp_Datafeed_Transfer_Uri $uri)
+    {
+    	$this->_uri = $uri;
+    	return $this;
+    }    
+    
     /**
      * Setter for options, used to configure the object by providing a set of options as an array
      * Valid values for the array depend on the object.
@@ -259,34 +173,6 @@ class Dfp_Datafeed_Transfer_Adapter_Ftp extends Dfp_Datafeed_Transfer_Adapter_Ab
     {
         $options = array_change_key_case($options);
 
-        if (isset($options['host'])) {
-            if (is_string($options['host'])) {
-                $this->setHost($options['host']);
-            } else {
-                throw new Dfp_Datafeed_Transfer_Exception('Invalid Host');
-            }
-        }
-        if (isset($options['port'])) {
-            if (is_numeric($options['port'])) {
-                $this->setPort($options['port']);
-            } else {
-                throw new Dfp_Datafeed_Transfer_Exception('Invalid Port');
-            }
-        }
-        if (isset($options['username'])) {
-            if (is_string($options['username'])) {
-                $this->setUsername($options['username']);
-            } else {
-                throw new Dfp_Datafeed_Transfer_Exception('Invalid Username');
-            }
-        }
-        if (isset($options['password'])) {
-            if (is_string($options['password'])) {
-                $this->setPassword($options['password']);
-            } else {
-                throw new Dfp_Datafeed_Transfer_Exception('Invalid Password');
-            }
-        }
         if (isset($options['basepath'])) {
             if (is_string($options['basepath'])) {
                 $this->setBasePath($options['basepath']);
@@ -322,19 +208,9 @@ class Dfp_Datafeed_Transfer_Adapter_Ftp extends Dfp_Datafeed_Transfer_Adapter_Ab
             $destination = $source;
         }
 
-        /**
-         * Here we change the error handler to catch the error message thrown by ftp_put
-         */
-        set_error_handler(array($this, 'errorHandler'), E_WARNING);
-        $put = ftp_put($this->_getFtp(), $destination, $sourceUri, FTP_BINARY);
-        restore_error_handler();
-
-        if (!$put) {
-            //$error = array_pop($this->getErrors());
-            $message = sprintf('File PUT failed for file from %s to %s', $sourceUri, $destination);
-            $this->addError($message);
-            throw new Dfp_Datafeed_Transfer_Adapter_Exception($message);
-        }
+        $this->_ftpConnect();
+        
+		$this->_ftpPut($destination, $sourceUri);
 
         return $this;
     }
@@ -350,58 +226,79 @@ class Dfp_Datafeed_Transfer_Adapter_Ftp extends Dfp_Datafeed_Transfer_Adapter_Ab
         // Destination to local file system
         $destUri = $this->getBasePath() . DIRECTORY_SEPARATOR . $destination;
 
-        /**
-         * Here we change the error handler to catch the error message thrown by ftp_put
-         */
-        set_error_handler(array($this, 'errorHandler'), E_WARNING);
-        $get = ftp_get($this->_getFtp(), $destUri, $source, FTP_BINARY);
-        restore_error_handler();
-
-        if (!$get) {
-            //$error = array_pop($this->getErrors());
-            $message = sprintf('File GET failed for file from %s to %s', $source, $destUri);
-            $this->addError($message);
-            throw new Dfp_Datafeed_Transfer_Adapter_Exception($message);
-        }
+        $this->_ftpConnect();
+        
+        $this->_ftpGet($destUri, $source);
+        
+        return $this;
     }
-
-    /**
-     * Get's an FTP connection.
-     *
-     * @throws Dfp_Datafeed_Transfer_Adapter_Exception
-     */
-    protected function _getFtp()
+    
+    protected function _ftpGet($dest, $source)
     {
-        if (!is_resource($this->_ftp)) {
+    	/**
+    	 * Here we change the error handler to catch the error message thrown by ftp_get
+    	 */
+    	set_error_handler(array($this, 'errorHandler'), E_WARNING);
+    	$get = ftp_get($this->_ftp, $dest, $source, FTP_BINARY);
+    	restore_error_handler();
+    	
+    	if (!$get) {
+    		$message = sprintf('File GET failed for file from %s to %s', $source, $destUri);
+    		$this->addError($message);
+    		throw new Dfp_Datafeed_Transfer_Adapter_Exception($message);
+    	}    	
+    }
+    
+    protected function _ftpPut($destination, $source)
+    {
+    	/**
+    	 * Here we change the error handler to catch the error message thrown by ftp_put
+    	 */
+    	set_error_handler(array($this, 'errorHandler'), E_WARNING);
+    	$put = ftp_put($this->_getFtp(), $destination, $source, FTP_BINARY);
+    	restore_error_handler();  
 
-            //set_error_handler(array($this, 'errorHandler'), E_WARNING);
-            $this->_ftp = ftp_connect($this->getHost(), $this->getPort(), $this->getTimeout());
-            //restore_error_handler();
-
-            if (!$this->_ftp) {
-                $message = sprintf('Conection failed to %s', $this->getHost());
-                $this->addError($message);
-                throw new Dfp_Datafeed_Transfer_Adapter_Exception($message);
-            }
-
-            set_error_handler(array($this, 'errorHandler'), E_WARNING);
-            $login = ftp_login($this->_ftp, $this->getUsername(), $this->getPassword());
-            restore_error_handler();
-
-            if (!$login) {
-                $message = sprintf(
-                    'Login failed to %s @ %s Port: %s, Using password: %s',
-                    $this->getUsername(), $this->getHost(), $this->getPort(),
-                    (('' != $this->getPassword()) ? 'Yes' : 'No')
-                );
-                $this->addError($message);
-                throw new Dfp_Datafeed_Transfer_Adapter_Exception($message);
-            }
-        }
-
-        return $this->_ftp;
+    	if (!$put) {
+    		//$error = array_pop($this->getErrors());
+    		$message = sprintf('File PUT failed for file from %s to %s', $sourceUri, $destination);
+    		$this->addError($message);
+    		throw new Dfp_Datafeed_Transfer_Adapter_Exception($message);
+    	}    	
     }
 
+    protected function _ftpConnect()
+    {
+    	if (is_resource($this->_ftp)) {
+    		ftp_close($this->_ftp);
+    	}
+    	
+    	set_error_handler(array($this, 'errorHandler'), E_WARNING);
+    	$this->_ftp = ftp_connect($this->getUri()->getHost(), $this->getUri()->getPort(), $this->getTimeout());
+    	restore_error_handler();   
+
+    	if (!$this->_ftp) {
+    		$message = sprintf('Conection failed to %s', $this->getUri()->getHost());
+    		$this->addError($message);
+    		throw new Dfp_Datafeed_Transfer_Adapter_Exception($message);
+    	}
+
+    	set_error_handler(array($this, 'errorHandler'), E_WARNING);
+    	$login = ftp_login($this->_ftp, $this->getUri()->getUsername(), $this->getUri()->getPassword());
+    	restore_error_handler();
+    	
+    	if (!$login) {
+    		$message = sprintf(
+    				'Login failed to %s @ %s Port: %s, Using password: %s',
+    				$this->getUri()->getUsername(), $this->getUri()->getHost(), $this->getUri()->getPort(),
+    				(('' != $this->getUri()->getPassword()) ? 'Yes' : 'No')
+    		);
+    		$this->addError($message);
+    		throw new Dfp_Datafeed_Transfer_Adapter_Exception($message);
+    	}  
+
+   		ftp_pasv($ftp_stream, $this->getPassive());
+    }
+    
     /**
      * Catches and records the error.
      *
